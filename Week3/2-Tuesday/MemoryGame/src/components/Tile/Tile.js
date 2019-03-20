2ac979777f9867749cc6a933bd9e14693580ff26
Fizban <grygielj@gmail.com>
2019-03-19 22:49:12 -0500

    Updated Memory Game

--- Week3/2-Tuesday/MemoryGame/src/components/Tile/Tile.js
+++ Week3/2-Tuesday/MemoryGame/src/components/Tile/Tile.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Tile.css';
 
+const TileKey = {
+  'stark': '../img/stark.png',
+  'lannister': '../img/lannister.png',
+  'baratheon': '../img/baratheon.png',
+  'martell': '../img/martell.png',
+  'greyjoy': '../img/greyjoy.png',
+  'targaryen': '../img/targaryen.png',
+}
+
 const Tile = (props) => {
 
   return (
     <div className='tile' id={props.id} onClick={(e) => props.updateTile(e)}>
-      {!props.status ? <h1>{props.value}</h1> : null}
+      {/*!props.status ? <h1>{props.value}</h1> : null*/}
+      {!props.status ? <img src={TileKey[props.value]} alt={props.value}/> : null}
     </div>
   );
 }
